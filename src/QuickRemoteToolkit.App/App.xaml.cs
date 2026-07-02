d366532330609af3e573218f7174004aea80ba6a
KovylyaevMN <admin@gt-logistics.su>
2026-07-02 17:26:12 +0500

    Register Windows code pages

--- src/QuickRemoteToolkit.App/App.xaml.cs
+++ src/QuickRemoteToolkit.App/App.xaml.cs
@@ -1,7 +1,13 @@
 using System.Windows;
+using System.Text;
 
 namespace QuickRemoteToolkit.App;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        base.OnStartup(e);
+    }
 }
